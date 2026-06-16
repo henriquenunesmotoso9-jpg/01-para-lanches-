a01a5eb5977a21fdf340438f16aa706edf8cd58b
HENRIQUE <henrique.nunes.motoso9@gmail.com>
2026-06-16 10:50:15 -0300

    Add login verification and password visibility toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 
+function verificarlogin() {
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if (login == "Henrique" && senha == "21082009Ri@") {
+        alert("Login correto! Bem-vindo ao Parah Lanches");
+    } else {
+        alert("Login ou senha incorretos");
+    }
+}
+
+function mostrarSenha() {
+    var campo = document.getElementById("senha");
+
+    if (campo.type === "password") {
+        campo.type = "text";
+    } else {
+        campo.type = "password";
+    }
+}
+
